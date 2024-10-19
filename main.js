d5bbec6129f5ab8ba8eae225c00fd6e63c10d4f4
hassan ali <hassanali0111272@gmail.com>
2024-10-19 10:38:57 +0500

    done

--- main.js
+++ main.js
@@ -4,82 +4,71 @@ const todoWrapper = document.querySelector(".todo-wrapper");
 const todoItem = document.querySelectorAll(".item");
 const searchItems = document.querySelector(".search-container input");
 const search = document.querySelector("#search");
+let index;
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if(inputElem.value){
-    
-    let newTodo = document.createElement("li");
-    newTodo.classList.add("item");
-    todoContext = `<div class="text">${inputElem.value}</div>
+  if (index === 0 || index) {
+    for (let i = 0; i < todoWrapper.children.length; i++) {
+      if (index == i) {
+        todoWrapper.children[i].firstElementChild.textContent = inputElem.value;
+      }
+    }
+  } else {
+    if (inputElem.value) {
+      let newTodo = document.createElement("li");
+      newTodo.classList.add("item");
+      todoContext = `<div class="text">${inputElem.value}</div>
             <div class="btns">
               <button class="edit">Edit</button>
               <button class="remove">Delete</button>
               <button class="done">Done</button>
             </div>`;
-    newTodo.innerHTML = todoContext;
-    todoWrapper.appendChild(newTodo);
-    console.log(todoWrapper);
+      newTodo.innerHTML = todoContext;
+      todoWrapper.appendChild(newTodo);
 
-    inputElem.value = "";
+
+    }
   }
-  
+  inputElem.value = "";
 });
 todoWrapper.addEventListener("click", function (e) {
   if (e.target.classList.contains("remove")) {
-    e.target.parentNode.parentNode.remove();
+    e.target.parentElement.parentElement.remove();
   }
-
-  // if (e.target.parentNode.parentNode.children[0].classList.contains("cut")) {
-  //   e.target.parentNode.parentNode.children[0].classList.remove("cut");
-  // }
-
   if (e.target.classList.contains("done")) {
-    if (
-      e.target.parentNode.parentNode.children[0].style.textDecoration ==
-      "line-through"
-    ) {
-      e.target.parentNode.parentNode.children[0].style.textDecoration = "none";
-    } else {
-      e.target.parentNode.parentNode.children[0].style.textDecoration =
-        "line-through";
+    if(e.target.parentElement.previousElementSibling.classList.contains("cut")){
+      e.target.parentElement.previousElementSibling.classList.remove("cut");
+    } else{
+      e.target.parentElement.previousElementSibling.classList.add("cut");
     }
   }
   if (e.target.classList.contains("edit")) {
-    let editText = e.target.parentElement.previousElementSibling.textContent.trim();
-    // console.log(todoWrapper.children.length)
-    debugger;
+    let editText =
+      e.target.parentElement.previousElementSibling.textContent.trim(); 
     let valueToUpdate;
-    for(let i =0; i < todoWrapper.children.length; i++){
-    valueToUpdate = todoWrapper.children[i].firstElementChild.textContent;
-    if(valueToUpdate.trim() == inputElem.value.trim()){
-      console.log(i)
+
+    for (let i = 0; i < todoWrapper.children.length; i++) {
+      valueToUpdate = todoWrapper.children[i].firstElementChild.textContent;
+
+      if (valueToUpdate == editText) {
+        index = i;
+      }
     }
-    }
-    // e.target.parentNode.parentNode.children[0].setAttribute(
-    //   "contenteditable","");
-    //   inputElem.textContent = e.target.parentElement.parentElement[0];
-    //   console.log(inputElem.textContent)
-    // // e.target.parentNode.parentNode.children[0].textContent = "";
-    // e.target.parentNode.parentNode.children[0].focus();
-    // e.target.parentNode.parentNode.children[0].addEventListener(
-    //   "blur",
-    //   function (e) {
-    //     e.target.parentNode.parentNode.children[0].removeAttribute(
-    //       "contenteditable"
-    //     );
-    //     e.target.parentNode.parentNode.children[0].textContent;
-    //   }
-    // );
+    inputElem.value = editText;
+    inputElem.focus();
   }
 });
 function filteredTodos(enteredText) {
-
-  for(let i = 0; i <todoWrapper.children.length; i++){
-      if(!todoWrapper.children[i].firstElementChild.textContent.toLowerCase().includes(enteredText)){
-        todoWrapper.children[i].classList.add("filterItem");
-      } else{
-         todoWrapper.children[i].classList.remove("filterItem");
-      }
+  for (let i = 0; i < todoWrapper.children.length; i++) {
+    if (
+      !todoWrapper.children[i].firstElementChild.textContent
+        .toLowerCase()
+        .includes(enteredText)
+    ) {
+      todoWrapper.children[i].classList.add("filterItem");
+    } else {
+      todoWrapper.children[i].classList.remove("filterItem");
+    }
   }
 }
 searchItems.addEventListener("keyup", function (e) {
